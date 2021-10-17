5ad0f1461878059488692e8fc772d2a2aaaa9a52
tlnkzl <tulin.kuzulu@gmail.com>
2021-10-17 17:21:33 -0400

    Initial commit

---
+++ src/pages/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../components/Button';
+
+export default function Page() {
+  return (
+    <div>
+      <h2> I am the Page</h2>
+      <Button />
+    </div>
+  );
+}
